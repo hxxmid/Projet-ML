b128f7201b089edfb4d75a68a03b06c40d61d6ee
Hamid <hamid.zidelmal@supdevinci-edu.fr>
2024-03-29 09:38:30 +0100

    ajoute graphe

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 model = pk.load(open('model.pkl','rb'))
 
-st.header('Car Price Prediction ML Model')
+st.header('Prévision du prix des voitures')
 
 cars_data = pd.read_csv('Cardetails.csv')
 
@@ -14,17 +14,17 @@ def get_brand_name(car_name):
     return car_name.strip()
 cars_data['name'] = cars_data['name'].apply(get_brand_name)
 
-name = st.selectbox('Select Car Brand', cars_data['name'].unique())
-year = st.slider('Car Manufactured Year', 1994,2024)
-km_driven = st.slider('No of kms Driven', 11,200000)
-fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
-seller_type = st.selectbox('Seller  type', cars_data['seller_type'].unique())
-transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
-owner = st.selectbox('Seller  type', cars_data['owner'].unique())
-mileage = st.slider('Car Mileage', 10,40)
-engine = st.slider('Engine CC', 700,5000)
-max_power = st.slider('Max Power', 32,200)
-seats = st.slider('No of Seats', 5,10)
+name = st.selectbox('Sélectionnez la marque de la voiture', cars_data['name'].unique())
+year = st.slider('Année de fabrication de la voiture', 1994,2024)
+km_driven = st.slider('Nombre de km parcourus', 11,200000)
+fuel = st.selectbox('Type de carburant', cars_data['fuel'].unique())
+seller_type = st.selectbox('Type de vendeur', cars_data['seller_type'].unique())
+transmission = st.selectbox('Type de transmission', cars_data['transmission'].unique())
+owner = st.selectbox('Nombre de mains', cars_data['owner'].unique())
+mileage = st.slider('Kilométrage de la voiture', 10,40)
+engine = st.slider('Moteur CC', 700,5000)
+max_power = st.slider('Maximum d''énergie', 32,200)
+seats = st.slider('Nombre de sièges', 5,10)
 
 
 if st.button("Predict"):
